1b1afab30c02af136e44e0deb8eec0bf2f00377c
William Abelo <william.abelo@outlook.com>
2017-08-16 16:19:04 -0300

    primeiro commit

---
+++ app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.Component';
+import { ContatosModule } from './contatos/contatos.module';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        ContatosModule
+    ],
+    declarations: [AppComponent],
+    bootstrap: [AppComponent]
+})
+
+export class AppModule {}
